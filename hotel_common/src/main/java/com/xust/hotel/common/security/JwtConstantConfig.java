9b322f30acd271bceb8d6146dafcafef1131636e
noShampoo <2551480456@qq.com>
2020-12-31 22:02:44 +0800

    添加JWT相关工具

---
+++ hotel_common/src/main/java/com/xust/hotel/common/security/JwtConstantConfig.java
@@ -0,0 +1,24 @@
+package com.xust.hotel.common.security;
+
+/**
+ * @author bhj
+ */
+public class JwtConstantConfig {
+
+    public static final String HEADER_KEY = "Authorization";
+
+    public static final String HEADER_TOKEN_START = "bWy7Gz2SXouST2";
+
+    public static final String HEADER_TOKEN_END = "bwyGZSzwYbsg";
+
+    public static final int SUB_START_NUM = 14;
+
+    public static final int SUB_END_NUM = 12;
+
+    public static final String USER_ROLE_ADMIN = "admin";
+
+    public static final String USER_ROLE_NORMAL = "normal";
+
+    public static final String REQ_ATTRIBUTE_KEY = "login-info";
+
+}
